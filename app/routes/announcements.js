e1811d7c04df434697939f97303eb20ab3963f63
Darius Capizzi and Ming Wen <darius+ming@>
2016-06-06 16:27:50 -0700

    Add delete announcements functionality

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return Ember.RSVP.hash({
-      rentals: this.store.findAll('rental'),
-      announcements: this.store.findAll('announcement')
-    });
+    return this.store.findAll('announcement')
   },
+  actions: {
+  destroyAnnouncement(announcement) {
+    announcement.destroyRecord();
+    this.transitionTo('index');
+    }
+  }
 });
