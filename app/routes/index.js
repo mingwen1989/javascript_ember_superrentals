b2cce14e1207c71608ac4afb6a28dcfc7a0e79c0
themingfu <mwen82@gmail.com>
2016-06-07 08:58:55 -0700

    Fix announcement tile

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,12 +2,24 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-     return this.store.findAll('rental');
-   },
-   actions: {
+      return Ember.RSVP.hash({
+      rentals: this.store.findAll('rental'),
+      announcements: this.store.findAll('announcement')
+      });
+    },
+  actions: {
     save3(params) {
       var newRental = this.store.createRecord('rental', params);
       newRental.save();
+      this.transitionTo('index');
+    },
+    announcementSave(params) {
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('index');
+    },
+    destroyAnnouncement(announcement) {
+      announcement.destroyRecord();
       this.transitionTo('index');
     }
   }
